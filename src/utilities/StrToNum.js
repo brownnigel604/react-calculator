f772b8fa4c069f8f97a7d7c75abc3f393f6a125e
akbcit <kumaraditya.ca@gmail.com>
2023-11-30 22:11:08 -0800

    bonus 3 commit 3 empty decimal bug fixed for operand 2

--- src/utilities/StrToNum.js
+++ src/utilities/StrToNum.js
@@ -19,26 +19,29 @@
 
 // export default StrToNum;
 const StrToNum = (inputStr) => {
-    console.log(`StrToNum: ${typeof inputStr} ${inputStr}`);
-    if (inputStr === "") {
-      return 0;
-    }
-    // Try to parse as integer
-    let integerResult = parseInt(inputStr);
-  
-    // Try to parse as float
-    let floatResult = parseFloat(inputStr);
-  
-    // Check if there is anything after the decimal point
-    let hasDecimal = false;
-    if (typeof inputStr === "string") {
-      hasDecimal = inputStr.includes(".");
-    }
-  
-    // Determine the result based on whether it has a decimal or not
-    let result = hasDecimal ? floatResult : integerResult;
-  
-    return result;
-  };
-  
-  export default StrToNum;
+  if (inputStr === ".") {
+    return 0;
+  }
+  console.log(`StrToNum: ${typeof inputStr} ${inputStr}`);
+  if (inputStr === "") {
+    return 0;
+  }
+  // Try to parse as integer
+  let integerResult = parseInt(inputStr);
+
+  // Try to parse as float
+  let floatResult = parseFloat(inputStr);
+
+  // Check if there is anything after the decimal point
+  let hasDecimal = false;
+  if (typeof inputStr === "string") {
+    hasDecimal = inputStr.includes(".");
+  }
+
+  // Determine the result based on whether it has a decimal or not
+  let result = hasDecimal ? floatResult : integerResult;
+
+  return result;
+};
+
+export default StrToNum;
